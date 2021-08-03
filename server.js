0870dd3ebfc86f96bc0558ec8bb4b96fc0f7e182
Rex Yu <yu19910513@gmail.com>
2021-08-02 23:03:36 -0700

    url

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  process.env.MONGODB_URI || 'mongodb+srv://yu19910513:Lin19920918@cluster0.hhwzd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useFindAndModify: false
